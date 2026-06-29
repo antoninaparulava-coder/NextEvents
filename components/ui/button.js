add07969eb625b7826e9326af59716583c99d5c3
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-06-29 18:51:14 +0400

    Adding a Filter Form for Filtering Events

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,11 +2,18 @@ import Link from "next/link"
 import classes from './button.module.css'
 
 function Button(props) {
-    return(
+    if(props.link){
+        return(
         <Link href={props.link} className={classes.btn}>
             {props.children}
         </Link>
+        );
+    }
+
+    return(
+        <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
     )
+    
 }
 
 export default Button
