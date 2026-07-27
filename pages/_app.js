54c0de3cae9644473a993b72aa8416eaf99fb89d
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-07-27 20:27:05 +0400

    add Notification

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import Layout from '../components/layout/layout';
 import Head from "next/head";
+import Notification from "@/components/notification/notification";
 
 export default function App({ Component, pageProps }) {
   return(
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test" status="error" />
     </Layout>
 ) 
 }
