261e0ac7d5826d2fa635256b85e604a9bb5fdd1a
antoninaparulava-coder <antoninaparulava@gmail.com>
2026-07-29 18:17:36 +0400

    Creating a New React Context

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,21 @@ import "@/styles/globals.css";
 import Layout from '../components/layout/layout';
 import Head from "next/head";
 import Notification from "@/components/notification/notification";
+import { NotificationContextProvider } from "@/store/notification-context";
 
 export default function App({ Component, pageProps }) {
   return(
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="NextJS Events" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-      </Head>
-      <Component {...pageProps} />
-      <Notification title="Test" message="This is a test" status="error" />
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="Test" message="This is a test" status="error" />
     </Layout>
+    </NotificationContextProvider>
+    
 ) 
 }
---
+++ store/notification-context.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+const NotificationContext = createContext({
+    notification: null, // { title, message, status}
+    showNotification: function() {},
+    hideNotification: function() {},
+});
+
+export function NotificationContextProvider(props){
+
+    return (
+    <NotificationContext.Provider>
+        {props.children}
+    </NotificationContext.Provider>
+    );
+}
+
+export default NotificationContext;
